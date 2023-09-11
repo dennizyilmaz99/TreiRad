8cb40dadce9fc983c3b5da83955ff8a69e5dedea
dator <dennizyilmaz1903@gmail.com>
2023-09-11 14:51:23 +0200

    Initial Commit

---
+++ TreiRad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TreiRad
+//
+//  Created by dator on 2023-09-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
